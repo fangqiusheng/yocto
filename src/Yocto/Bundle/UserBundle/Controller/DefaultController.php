971f6b47f82e31a756b74f4547df2b800ed79e47
krisrybak <kris.rybak@hotmail.co.uk>
2013-07-21 08:14:12 +0100

    Updated security layer to accept users from memory and db

--- src/Yocto/Bundle/UserBundle/Controller/DefaultController.php
+++ src/Yocto/Bundle/UserBundle/Controller/DefaultController.php
@@ -6,14 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Yocto\Bundle\UserBundle\Entity\User;
+
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/hello")
      * @Template()
      */
     public function indexAction($name)
     {
-        return array('name' => $name);
+        return array();
     }
 }
