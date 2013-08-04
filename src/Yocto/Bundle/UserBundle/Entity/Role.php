a13e3c06b6050d293d04d365612db589c7168915
krisrybak <kris.rybak@hotmail.co.uk>
2013-08-04 10:41:58 +0100

    Removes old repostiory and entities. Adds new Group entity

--- src/Yocto/Bundle/UserBundle/Entity/Role.php
+++ src/Yocto/Bundle/UserBundle/Entity/Role.php
@@ -5,6 +5,7 @@ namespace Yocto\Bundle\UserBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\Role\RoleInterface;
+use Yocto\Bundle\UserBundle\Entity\Group;
 
 /**
  * @ORM\Table(name="acl_roles")
