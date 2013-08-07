3119bdeeb2afedd00d1e6c644c097a7a30c6cdf3
krisrybak <kris.rybak@hotmail.co.uk>
2013-08-07 09:16:34 +0000

    Sets security requirements required to render menu item

--- src/Yocto/Bundle/LayoutBundle/Menu/Builder.php
+++ src/Yocto/Bundle/LayoutBundle/Menu/Builder.php
@@ -44,7 +44,7 @@ class Builder extends ContainerAware
             ->dispatch(BundleEvent::EVENT, new BundleEvent())
             ->getBundles();
 
-        // Basic setup
+        // Basic set up
         $this->factory  = $factory;
         $this->security = $this->container->get('security.context');
 
@@ -55,14 +55,18 @@ class Builder extends ContainerAware
 
         // Add Menu items that are available for user
         foreach($bundles as $bundle) {
-            if ($this->security->isGranted($bundle['role'])) {
+            // We only want add bundles if the user satisfies the minimum role requirements
+            // which can also be unspecified in case if no special requirements are needed
+            // in order to access the bundle (i.e. dashboard?)
+            if (!isset($bundle['role']) || $this->security->isGranted($bundle['role'])) {
 
-                // Only add the menu if user has granted at least minimum access
+                // Add item to the menu
                 $menu->addChild($bundle['name'], array(
                     'route' => 'default_dashboard',
                     'uri'   => '#dashboard'
                 ));
             }
+
         }
 
         return $menu;
