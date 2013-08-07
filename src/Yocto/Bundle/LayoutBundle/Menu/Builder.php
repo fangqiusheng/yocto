d5b7f39e9131a0f1afd57bbfa9160e751bfe532e
krisrybak <kris.rybak@hotmail.co.uk>
2013-08-07 13:14:13 +0000

    Adds function to sort bundles bu their priority

--- src/Yocto/Bundle/LayoutBundle/Menu/Builder.php
+++ src/Yocto/Bundle/LayoutBundle/Menu/Builder.php
@@ -53,6 +53,9 @@ class Builder extends ContainerAware
             ->factory
             ->createItem('root');
 
+        // Sort the list
+        $this->sortBundles($bundles);
+
         // Add Menu items that are available for user
         foreach($bundles as $bundle) {
             // We only want add bundles if the user satisfies the minimum role requirements
@@ -70,5 +73,50 @@ class Builder extends ContainerAware
         }
 
         return $menu;
+    }
+
+    /**
+     * This function allows as to order our menu accordingly to priority list
+     * @param   Array $bundles     Array of bundles to sort
+     * @return  Array $ordered     Ordered array of bundles by their priority
+     */
+    private function sortBundles(array $bundles)
+    {
+        // First let's create some space for sorting items
+        // havePrioritySet will store elements that had priority set
+        $havePrioritySet = array();
+
+        // Now some items may not have any priority option set at all
+        // We will add them to the end of the list
+        $noPrioritySet = array();
+
+        // Our new ordered list of bundles
+        $ordered = array();
+
+        foreach ($bundles as $bundle) {
+            if (isset($bundle['priority'])) {
+                array_push(
+                    $havePrioritySet,
+                    array('priority' => $bundle['priority'], 'bundle' => $bundle)
+                );
+            } else {
+                array_push($noPrioritySet, $bundle);
+            }
+        }
+
+        // Sort both prioritised and non-prioritised collections
+        asort($havePrioritySet);
+        asort($noPrioritySet);
+
+        // Loop through our collections and add them to the list
+        foreach ($havePrioritySet as $prioritised) {
+            array_push($ordered, $prioritised['bundle']);
+        }
+
+        foreach ($noPrioritySet as $nonPrioritised) {
+            array_push($ordered, $nonPrioritised);
+        }
+
+        return $ordered;
     }
 }
