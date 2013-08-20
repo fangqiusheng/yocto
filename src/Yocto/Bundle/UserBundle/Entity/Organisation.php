526d85d0c5f5255e2a5286d44d43cab45be94590
krisrybak <kris.rybak@hotmail.co.uk>
2013-08-20 07:30:17 +0000

    Adds Organisations getters and setters

--- src/Yocto/Bundle/UserBundle/Entity/Organisation.php
+++ src/Yocto/Bundle/UserBundle/Entity/Organisation.php
@@ -27,4 +27,60 @@ class Organisation
      * @ORM\Column(name="is_active", type="boolean")
      */
     private $isActive;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Organisation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return Organisation
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
 }
