6090d0c39df20470c1dbb2fdd79746b2e9245578
krisrybak <kris.rybak@hotmail.co.uk>
2013-08-20 07:25:55 +0000

    Adds organisation properties: id, name, isActive

--- src/Yocto/Bundle/UserBundle/Entity/Organisation.php
+++ src/Yocto/Bundle/UserBundle/Entity/Organisation.php
@@ -5,7 +5,26 @@ namespace Yocto\Bundle\UserBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Table(name="acl_organisations")
+ * @ORM\Entity()
+ */
 class Organisation
 {
-    
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
 }
